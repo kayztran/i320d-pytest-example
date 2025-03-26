b46dcf084a02a61e4faf6d50e1a17abef3d77542
Kayla <ktran7292@utexas.edu>
2025-03-26 16:39:02 -0500

    these tests should not work (again)

--- my_code.py
+++ my_code.py
@@ -1,9 +1,7 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
-    
-  if (len(phone_num_to_fix) != 10):
-    raise ValueError("Can only format numbers that are exactly 10 digits long")  
+     
     
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
@@ -21,12 +19,12 @@ def test_fix_phone_num():
   assert fix_phone_num("5554429876") == '(555) 442 9876' 
                    
   assert fix_phone_num("3216543333") == '(321) 654 3333' 
-  
-  with pytest.raises(ValueError):
-        fix_phone_num("51")
-
+ 
 
 
 def test_2(): 
   assert fix_phone_num("555-442-98761") == '(555) 442 98761'        
   assert fix_phone_num("(321) 654 3333") == '(321) 654 3333' 
+    
+
+    
