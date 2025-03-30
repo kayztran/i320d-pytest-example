d72b86dd4e0bd10b55a038213e91294b964b95e2
Kayla <ktran7292@utexas.edu>
2025-03-30 17:52:22 -0500

    raise ValueError to explain why tests are failing

--- my_code.py
+++ my_code.py
@@ -1,7 +1,13 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
-     
+    
+  for ch in phone_num_to_fix: 
+    if not ch.isdigit():
+        raise ValueError("Can only format numbers")
+        
+  if (len(phone_num_to_fix) != 10):
+    raise ValueError("Can only format numbers that are exactly 10 digits long")
     
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
@@ -21,10 +27,15 @@ def test_fix_phone_num():
   assert fix_phone_num("3216543333") == '(321) 654 3333' 
  
 
-
-def test_2(): 
-  assert fix_phone_num("555-442-98761") == '(555) 442 98761'        
+def test_will_fail():   
+  #assert fix_phone_num("555-442-98761") == '(555) 442 98761'   
   assert fix_phone_num("(321) 654 3333") == '(321) 654 3333' 
-    
 
-    
+  
+def test_raise_ValueErrors_for_invalid_inputs():
+    with pytest.raises(ValueError):
+        fix_phone_num("55544298761") # too long
+        
+    with pytest.raises(ValueError):
+        fix_phone_num("555-442-98761") #non digit characters
+        
